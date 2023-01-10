d31dbee28254dc08c74a9133ccdf626838ead58d
Raphaël <rapha.78@live.fr>
2023-01-10 18:05:39 +0100

    Ajout

--- projetloupgarou/src/Controller/HomeController.php
+++ projetloupgarou/src/Controller/HomeController.php
@@ -2,18 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
+        return $this->render('index.html.twig',['controller_name'=>'HomeController']);
     }
 }
