3e26a629f4da58bd8f4b412d7390aa982e6f6c70
Raphaël <rapha.78@live.fr>
2023-01-11 11:34:20 +0100

    Index modif + ajout page form

--- projetloupgarou/src/Controller/HomeController.php
+++ projetloupgarou/src/Controller/HomeController.php
@@ -8,9 +8,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    # page d'accueil
     #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('index.html.twig',['controller_name'=>'HomeController']);
+    }
+
+    #[Route('/formulaire', name: 'formulaire')]
+    public function formulaire(): Response
+    {
+        return $this->render('formulaire.html.twig',['controller_name'=>'HomeController']);
     }
 }
