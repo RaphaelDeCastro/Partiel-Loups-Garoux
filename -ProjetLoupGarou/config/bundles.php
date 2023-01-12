5a16b2357c143ecb27edc249dbecab87d4cedd41
Noé Douguet <noedouguet@gmail.com>
2023-01-12 17:01:09 +0100

    Début methode
    methode verification role

--- -ProjetLoupGarou/config/bundles.php
+++ -ProjetLoupGarou/config/bundles.php
@@ -3,5 +3,8 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+<<<<<<< Updated upstream
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+=======
+>>>>>>> Stashed changes
 ];
