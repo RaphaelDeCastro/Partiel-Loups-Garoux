2e30aed34904b60dc6cdade97733279825cb4ba8
Raphaël <rapha.78@live.fr>
2023-01-12 11:03:11 +0100

    Ajout index + formulaire

---
+++ -ProjetLoupGarou/src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig',[
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/formulaire', name: 'formulaire')]
+    public function formulaire(): Response
+    {
+        return $this->render('formulaire.html.twig',[
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
